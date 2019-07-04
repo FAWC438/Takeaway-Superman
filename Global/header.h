95b1111093aa9965ddd3930c4364292e29c4667d
Kevin Ling <qq185030406@live.com>
2019-07-04 14:01:11 +0800

    第一次修改

--- Global/header.h
+++ Global/header.h
@@ -9,6 +9,8 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 //#include<algorithm>
 #include "consts.h"
 #include "structure.h"
--- main.c
+++ main.c
@@ -1,8 +1,4 @@
 #include "./Global/header.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 
 int main()
 {
