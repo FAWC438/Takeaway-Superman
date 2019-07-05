d513dc8066bc35b3e9efd83dfeca2f675fcc0f10
Kevin Ling <qq185030406@live.com>
2019-07-05 16:50:07 +0800

    2019.7.5 16.50 骑手hire函数修改

--- Functions/riderFunctions.h
+++ Functions/riderFunctions.h
@@ -15,7 +15,10 @@ int hireRider()
         p = p->Nxt_rider;
     }
     // 初始化新骑手
-    newRider->id = p->Cur_rider->id + 1;
+    if (p->Cur_rider)
+        newRider->id = p->Cur_rider->id + 1;
+    else
+        newRider->id = 1;
     newRider->rider_x = COMPANY_X;
     newRider->rider_y = COMPANY_Y;
     newRider->Bag = creatOrderList(); // 骑手的背包是一个订单链表
@@ -72,7 +75,7 @@ void riderPos(int rider_id, int *x, int *y)
     *y = p->Cur_rider->rider_y;
 }
 /*
-    入骑手id和目的地（房子坐标），根据骑手当前位置，先左右,后上下，移动一个单位
+    根据骑手id和目的地（房子坐标），根据骑手当前位置，先左右,后上下，移动一个单位
     int rider_id:骑手id
     int pos_x:目的地横坐标
     int pos_y:目的地纵坐标
@@ -89,40 +92,40 @@ void riderMove(int rider_id, int pos_x, int pos_y)
     *cur_y = p->Cur_rider->rider_y;
     // TODO: 测试移动单元
     // 先左右
-    if(i % 2 == 1 && j % 2 == 0)    // 在横向道路上
+    if (i % 2 == 1 && j % 2 == 0) // 在横向道路上
     {
-        if(*cur_y > pos_y + 1) // 在右边就要往左走
+        if (*cur_y > pos_y + 1) // 在右边就要往左走
         {
             (*cur_y) -= 2;
         }
-        else if(*cur_y < pos_y - 1) // 在左边要往右走
+        else if (*cur_y < pos_y - 1) // 在左边要往右走
         {
             (*cur_y) += 2;
         }
     }
-    else if(i % 2 == 0 && j % 2 == 1)   // 在纵向道路上
+    else if (i % 2 == 0 && j % 2 == 1) // 在纵向道路上
     {
-        if(*cur_y > pos_y + 1) // 在右边就要往左走
+        if (*cur_y > pos_y + 1) // 在右边就要往左走
         {
-            if(pos_x >= *cur_x)  // 目标在下或同行
+            if (pos_x >= *cur_x) // 目标在下或同行
             {
                 (*cur_x)++;
                 (*cur_y)--;
             }
-            else if(pos_x < *cur_x) // 目标在上
+            else if (pos_x < *cur_x) // 目标在上
             {
                 (*cur_x)--;
                 (*cur_y)--;
             }
         }
-        if(*cur_y < pos_y + 1) // 在右边就要往左走
+        if (*cur_y < pos_y + 1) // 在右边就要往左走
         {
-            if(pos_x >= *cur_x)  // 目标在下或同行
+            if (pos_x >= *cur_x) // 目标在下或同行
             {
                 (*cur_x)++;
                 (*cur_y)++;
             }
-            else if(pos_x < *cur_x) // 目标在上
+            else if (pos_x < *cur_x) // 目标在上
             {
                 (*cur_x)--;
                 (*cur_y)++;
@@ -130,53 +133,53 @@ void riderMove(int rider_id, int pos_x, int pos_y)
         }
     }
     // 后上下
-    if(i % 2 == 0 && j % 2 == 1)    // 在纵向道路上
+    if (i % 2 == 0 && j % 2 == 1) // 在纵向道路上
     {
-        if(*cur_x > pos_x + 1) // 在下边就要往上走
+        if (*cur_x > pos_x + 1) // 在下边就要往上走
         {
             (*cur_x) -= 2;
         }
-        else if(*cur_x < pos_x - 1) // 在上边要往下走
+        else if (*cur_x < pos_x - 1) // 在上边要往下走
         {
             (*cur_x) += 2;
         }
     }
-    else if(i % 2 == 0 && j % 2 == 1)   // 在横向道路上
+    else if (i % 2 == 0 && j % 2 == 1) // 在横向道路上
     {
-        if(*cur_x > pos_x + 1) // 在下边就要往上走
+        if (*cur_x > pos_x + 1) // 在下边就要往上走
         {
-            if(pos_y >= *cur_y)  // 目标在右或同列
+            if (pos_y >= *cur_y) // 目标在右或同列
             {
                 (*cur_y)++;
                 (*cur_x)--;
             }
-            else if(pos_y < *cur_y) // 目标在左
+            else if (pos_y < *cur_y) // 目标在左
             {
                 (*cur_y)--;
                 (*cur_x)--;
             }
         }
-        if(*cur_x < pos_x + 1) // 在上边就要往下走
+        if (*cur_x < pos_x + 1) // 在上边就要往下走
         {
-            if(pos_y >= *cur_y)  // 目标在右或同列
+            if (pos_y >= *cur_y) // 目标在右或同列
             {
                 (*cur_y)++;
                 (*cur_x)++;
             }
-            else if(pos_y < *cur_y) // 目标在左
+            else if (pos_y < *cur_y) // 目标在左
             {
                 (*cur_y)--;
                 (*cur_x)++;
             }
         }
     }
-    else    // 异常情况处理
+    else // 异常情况处理
     {
         // TODO: 加入终止函数？
         printf("咋骑手跑到房子里了？\n");
         //exit(0);
     }
-    
+
     /* 
         横纵坐标一奇一偶的都是路，坐标都是偶数的是房间，坐标都是奇数的是路口
         房子上面和下面的路（横着的）：(偶，奇) 房子左面和右面的路（竖着的）：(奇，偶)
