5ce24f3d4368a8fea99a29b66b81c6670b643247
Kevin Ling <qq185030406@live.com>
2019-07-06 15:15:12 +0800

    2019.7.6 15:14 整理代码（未添加新功能）

--- Functions/algorithm.h
+++ Functions/algorithm.h
@@ -5,48 +5,44 @@
 int virtualRun(int *cur_x, int *cur_y, int pos_x, int pos_y)
 {
     int cnt = 0;
-    while(abs((*cur_x) - pos_x) > 1 || abs((*cur_y) - pos_y) > 1)
+    while (abs((*cur_x) - pos_x) > 1 || abs((*cur_y) - pos_y) > 1)
     {
         i = *cur_x;
         j = *cur_y;
         cnt++;
         // 先左右
-        if(*cur_y < pos_y - 1 || *cur_y > pos_y + 1)
+        if (*cur_y < pos_y - 1 || *cur_y > pos_y + 1)
         {
-            if(i % 2 == 1 && j % 2 == 0)    // 在横向道路上
+            if (i % 2 == 1 && j % 2 == 0) // 在横向道路上
             {
-                if(*cur_y > pos_y + 1) // 在右边就要往左走
-                {
+                if (*cur_y > pos_y + 1) // 在右边就要往左走
                     (*cur_y) -= 2;
-                }
-                else if(*cur_y < pos_y - 1) // 在左边要往右走
-                {
+                else if (*cur_y < pos_y - 1) // 在左边要往右走
                     (*cur_y) += 2;
-                }
             }
-            else if(i % 2 == 0 && j % 2 == 1)   // 在纵向道路上
+            else if (i % 2 == 0 && j % 2 == 1) // 在纵向道路上
             {
-                if(*cur_y > pos_y + 1) // 在右边就要往左走
+                if (*cur_y > pos_y + 1) // 在右边就要往左走
                 {
-                    if(pos_x >= *cur_x)  // 目标在下或同行
+                    if (pos_x >= *cur_x) // 目标在下或同行
                     {
                         (*cur_x)++;
                         (*cur_y)--;
                     }
-                    else if(pos_x < *cur_x) // 目标在上
+                    else if (pos_x < *cur_x) // 目标在上
                     {
                         (*cur_x)--;
                         (*cur_y)--;
                     }
                 }
-                else if(*cur_y < pos_y + 1) // 在右边就要往左走
+                else if (*cur_y < pos_y + 1) // 在右边就要往左走
                 {
-                    if(pos_x >= *cur_x)  // 目标在下或同行
+                    if (pos_x >= *cur_x) // 目标在下或同行
                     {
                         (*cur_x)++;
                         (*cur_y)++;
                     }
-                    else if(pos_x < *cur_x) // 目标在上
+                    else if (pos_x < *cur_x) // 目标在上
                     {
                         (*cur_x)--;
                         (*cur_y)++;
@@ -55,42 +51,38 @@ int virtualRun(int *cur_x, int *cur_y, int pos_x, int pos_y)
             }
         }
         // 后上下
-        else if(i < pos_x - 1 || i > pos_x + 1)
+        else if (i < pos_x - 1 || i > pos_x + 1)
         {
-            if(i % 2 == 0 && j % 2 == 1)    // 在纵向道路上
+            if (i % 2 == 0 && j % 2 == 1) // 在纵向道路上
             {
-                if(*cur_x > pos_x + 1) // 在下边就要往上走
-                {
+                if (*cur_x > pos_x + 1) // 在下边就要往上走
                     (*cur_x) -= 2;
-                }
-                else if(*cur_x < pos_x - 1) // 在上边要往下走
-                {
+                else if (*cur_x < pos_x - 1) // 在上边要往下走
                     (*cur_x) += 2;
-                }
             }
-            else if(i % 2 == 1 && j % 2 == 0)   // 在横向道路上
+            else if (i % 2 == 1 && j % 2 == 0) // 在横向道路上
             {
-                if(*cur_x > pos_x + 1) // 在下边就要往上走
+                if (*cur_x > pos_x + 1) // 在下边就要往上走
                 {
-                    if(pos_y >= *cur_y)  // 目标在右或同列
+                    if (pos_y >= *cur_y) // 目标在右或同列
                     {
                         (*cur_y)++;
                         (*cur_x)--;
                     }
-                    else if(pos_y < *cur_y) // 目标在左
+                    else if (pos_y < *cur_y) // 目标在左
                     {
                         (*cur_y)--;
                         (*cur_x)--;
                     }
                 }
-                if(*cur_x < pos_x + 1) // 在上边就要往下走
+                if (*cur_x < pos_x + 1) // 在上边就要往下走
                 {
-                    if(pos_y >= *cur_y)  // 目标在右或同列
+                    if (pos_y >= *cur_y) // 目标在右或同列
                     {
                         (*cur_y)++;
                         (*cur_x)++;
                     }
-                    else if(pos_y < *cur_y) // 目标在左
+                    else if (pos_y < *cur_y) // 目标在左
                     {
                         (*cur_y)--;
                         (*cur_x)++;
@@ -102,7 +94,7 @@ int virtualRun(int *cur_x, int *cur_y, int pos_x, int pos_y)
     return cnt;
 }
 
-int getDis(int a_x, int a_y, int b_x, int b_y)  // 获取两点之间的曼哈顿距离
+int getDis(int a_x, int a_y, int b_x, int b_y) // 获取两点之间的曼哈顿距离
 {
     return abs(a_x - b_x) + abs(a_y - b_y);
 }
@@ -116,17 +108,12 @@ int getPath(Rider *nowRider, Order *nowOrder)
     int sum = 0;
     int now_x = nowRider->rider_x, now_y = nowRider->rider_y, nxt_x, nxt_y;
     OrderList *nowBag = nowRider->Bag->Nxt_order;
-    while(nowBag)
+    while (nowBag)
     {
-        if(nowBag->Cur_order->status == 1)
-        {
+        if (nowBag->Cur_order->status == 1)
             sum += virtualRun(&now_x, &now_y, nowBag->Cur_order->rest_x, nowBag->Cur_order->rest_y);
-        }
-        else if(nowBag->Cur_order->status == 2)
-        {
-            
-            sum += virtualRun(&now_x, &now_y, nowBag->Cur_order->cust_x, nowBag->Cur_order->cust_y)
-        }
+        else if (nowBag->Cur_order->status == 2)
+            sum += virtualRun(&now_x, &now_y, nowBag->Cur_order->cust_x, nowBag->Cur_order->cust_y);
         nowBag = nowBag->Nxt_order;
     }
     sum += virtualRun(now_x, now_y, nowOrder->rest_x, nowOrder->rest_y);
