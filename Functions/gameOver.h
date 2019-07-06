349b3091c0b5a7f864a0567ee855f2e9046549af
Kevin Ling <qq185030406@live.com>
2019-07-06 17:24:22 +0800

    gameOver相关的TODO接入

--- Functions/gameOver.h
+++ Functions/gameOver.h
@@ -5,15 +5,27 @@
 
 /* 
     破产结算
-    以下列出调用该函数的情况：
+    以下列出调用该函数的情况(int reason)：
     0. 破产：资金为负数
     1. 拒单：超过DEAD_TIME个时间单位接单
     2. 恶意废单：第二次超时，即接单后，在FINE_SECOND_TIME时间单位内未送达
+    3. 订单状态错误
+    4. 输入格式错误
+    5. 程序运行异常
 */
-void gameOver()
+void gameOver(int reason)
 {
     printf("------------------------------------------------\n");
     printf("公司已经破产！！！\n");
+    printf("公司破产原因：");
+    if (reason == 0)
+        printf("破产\n");
+    else if (reason == 1)
+        printf("拒单\n");
+    else if (reason == 2)
+        printf("恶意废单\n");
+    else
+        printf("异常原因，错误代码为%d\n", reason);
     printf("本次公司所在地址:（%d，%d）\n", COMPANY_X, COMPANY_Y);
     printf("公司剩余现金数：%d\n", CompanyMoney);
     printf("公司骑手数量：%d\n", CompanyRiderCount);
@@ -54,7 +66,6 @@ void gameOver()
 /* 
     正常结算
     以下列出调用该函数的情况：
-    0. 输入文件无更多内容
     1. 鼠标操作手动结束游戏
 */
 
